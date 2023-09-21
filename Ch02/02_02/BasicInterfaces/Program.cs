5f128ae8c8f611394360d719028538a36caa688a
Ruth Waiganjo <ruthwaiganjo@Ruths-MacBook-Pro.local>
2023-09-21 06:52:30 +0300

    create interface & run proj with custome nuget file

--- Ch02/02_02/BasicInterfaces/Program.cs
+++ Ch02/02_02/BasicInterfaces/Program.cs
@@ -5,10 +5,17 @@ namespace BasicInterfaces
 {
     // TODO: Define an IStorable interface that provides the ability to load and
     // save the information for an object
-
+    // No access modifiers
+    // No logic within the methods
+    // Not possible to declare member variables but properties do the same thing
+    interface IStorable {
+        void Save();
+        void Load();
+        bool NeedsSave { get; set; }
+    }
 
     // TODO: Implement IStorable on the Document class
-    class Document
+    class Document: IStorable
     {
         private string name;
 
@@ -19,6 +26,17 @@ namespace BasicInterfaces
         }
 
         // TODO: Implement the IStorable interface methods and properties
+        public void Save(){
+            Console.WriteLine("Saving...");
+        }
+
+        public void Load(){
+            Console.WriteLine("Loading...");
+        }
+
+        public bool NeedsSave {
+            get; set;
+        }
     }
 
     class Program
@@ -27,6 +45,9 @@ namespace BasicInterfaces
             Document d = new Document("Test Document");
 
             // TODO: Exercise the IStorable interface
+            d.Load();
+            d.Save();
+            d.NeedsSave = false;
 
             Console.WriteLine("\nPress Enter to continue...");
             Console.ReadLine();
